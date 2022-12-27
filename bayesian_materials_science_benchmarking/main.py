c57b3e045666c8f3840f37f0e339db11b0e22505
Hud <hudwahab@gmail.com>
2022-12-27 14:46:53 -0700

    load data

--- bayesian_materials_science_benchmarking/main.py
+++ bayesian_materials_science_benchmarking/main.py
@@ -1,7 +1,9 @@
-from pathlib import Path
+import pandas as pd
+from config import DATA_PATH
 
 def main() -> None:
-    print('tada')
-
+    dataset_name = 'Crossed barrel'
+    raw_dataset = pd.read_csv(DATA_PATH / f"{dataset_name}_dataset.csv")
+    print(raw_dataset.head())
 if __name__ == "__main__":
     main()
