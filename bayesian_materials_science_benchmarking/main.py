25a2b6a45bcd8b1d0c7713e0a4bdcb9f3faf1373
Hud <hudwahab@gmail.com>
2022-12-27 14:56:08 -0700

    preprocess crossed-barrel data

--- bayesian_materials_science_benchmarking/main.py
+++ bayesian_materials_science_benchmarking/main.py
@@ -1,9 +1,23 @@
+import copy
 import pandas as pd
 from config import DATA_PATH
+
+
 
 def main() -> None:
     dataset_name = 'Crossed barrel'
     raw_dataset = pd.read_csv(DATA_PATH / f"{dataset_name}_dataset.csv")
-    print(raw_dataset.head())
+    feature_name = list(raw_dataset.columns)[:-1]
+    objective_name = list(raw_dataset.columns)[-1]
+    ds = copy.deepcopy(raw_dataset)
+    ds[objective_name] = -raw_dataset[objective_name].values
+
+    unique_ds = ds.groupby(feature_name)[objective_name].agg(lambda x: x.unique().mean())
+    unique_ds = (unique_ds.to_frame()).reset_index()
+
+    print(unique_ds)
+
+
+
 if __name__ == "__main__":
     main()
